31246d5758c5188101093824713c8b01d3dca3a9
shiroresn <32104174+shiroresn@users.noreply.github.com>
2018-12-14 15:37:06 +0530

    Create FirstProgram.php

---
+++ FirstProgram.php
@@ -0,0 +1,45 @@
+<html>
+   
+   <head>
+      <title>Hello World</title>
+   </head>
+   
+   <body>
+      <?php echo "Hello, World!";?><br />
+      <?php print "Hello Shubham!!!!";
+      
+      $car = "BMW";
+      print "My car name is $car";
+      $car = 99;
+      print "My car number is $car";
+      
+      if($car > 100)
+      {
+          print "Your car is Police Car";
+      }
+      else
+      {
+          print "You are thief";
+      }
+      
+      $i = 0;
+      
+      for($i = 0;$i<100;$i++)
+      {
+          if($i%2==0)
+          {
+               print "$i";
+          }
+                
+          
+          
+          ?><br /> <?php
+      }
+      
+      ?>
+      
+      
+      
+   </body>
+
+</html>
